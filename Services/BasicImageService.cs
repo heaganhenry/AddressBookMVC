244115105c608b5dc665d79d1aaa805080fe8fbb
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2021-11-18 12:45:29 -0400

    Initial commit

---
+++ Services/BasicImageService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AddressBookMVC.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace AddressBookMVC.Services
+{
+	public class BasicImageService : IImageService
+	{
+		public async Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file)
+		{
+			using MemoryStream memoryStream = new();
+			await file.CopyToAsync(memoryStream);
+			byte[] byteFile = memoryStream.ToArray();
+
+			return byteFile;
+		}
+
+		public string ConvertByteArrayToFile(byte[] fileData, string extension)
+		{
+			if (fileData is null) return string.Empty;
+
+			string imageBase64Data = Convert.ToBase64String(fileData);
+			return $"data:{extension};base64,{imageBase64Data}";
+		}
+	}
+}
---
+++ Services/Interfaces/IImageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AddressBookMVC.Services.Interfaces
+{
+	public interface IImageService
+	{
+		public Task<byte[]> ConvertFileToByteArrayAsync(IFormFile file);
+
+		public string ConvertByteArrayToFile(byte[] fileData, string extension);
+	}
+}
